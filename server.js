bbad335b5be3d7ccfaeedc8a6a150b728d55db00
Glitch (url-shortener-amirzamzam) <none>
2018-03-27 08:49:41 +0000

    🎁🚜 Checkpoint ./public/client.js:780437/24 ./server.js:780437/36 ./views/index.html:780437/146 ./public/style.css:780437/28

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const angular = require('angular');
 const app = express();
 
 
