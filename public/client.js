a6c6adec2dbb8440a0c1e40d6e86ba659225ccf5
Glitch (url-shortener-amirzamzam) <none>
2018-03-28 03:20:17 +0000

    🍭🚓 Checkpoint ./views/index.html:780437/2 ./public/client.js:282805/95:780437/6 ./chat.txt:282805/89

--- public/client.js
+++ public/client.js
@@ -1,6 +1,7 @@
+// so, I see what you're talking about. The editor says "angular" is not defined
 var app = angular.module('shortURLApp', []);
 
-app.controller('shortAPPCtrl', function($scope){
+app.controller('shortAppCtrl', function($scope){
 $scope.test = "Hello World";
 
 });
