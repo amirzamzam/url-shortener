fcbe4030f932e6fd603e3491af71d4888dabaf4b
Glitch (url-shortener-amirzamzam) <none>
2018-03-28 03:31:15 +0000

    👨‍❤️‍👨🚡 Checkpoint ./public/client.js:282805/490:780437/438 ./views/index.html:780437/3

--- public/client.js
+++ public/client.js
@@ -1,7 +1,13 @@
-// so, I see what you're talking about. The editor says "angular" is not defined
+// so, I see what you're talking about. The editor says "angular" is not defined.
+// After launching the app, I went to check the JS console
+// The JS console shows that angular is loading and is globally accessible
+// Hypothesis: since angular is loading in the app, then it's being included propertly, 
+// and the message about "anguglar" not being defined exists only in the editor
+// you are welcome!
 var app = angular.module('shortURLApp', []);
 
 app.controller('shortAppCtrl', function($scope){
 $scope.test = "Hello World";
 
 });
+
